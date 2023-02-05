e7a85488b66bf6d9016a6ead8e058dfcb1a21d10
pasaf <pasaf@FARHAD>
2023-02-05 12:27:08 +0400

    Deploy 1

---
+++ Classbook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace book_class
+{
+    internal class Book
+    {
+        internal string name, Autname;
+        internal int PubDate, PageCount;
+
+        public override string ToString()
+        {
+            return $"{Autname} ''{name}'' {PubDate}";
+        }
+
+    }
+}
